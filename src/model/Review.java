dc9ec1b0bd2b264a4cbf15530d8d5577ac05d0bc
Aurélien Prieur <aurel_000@clint>
2014-12-22 11:10:37 +0100

    Add rank field in review table

--- src/model/Review.java
+++ src/model/Review.java
@@ -1,6 +1,9 @@
 package model;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Date;
 
@@ -8,18 +11,42 @@ public class Review {
 	private int id = 0;
 	private String title = "";
 	private String text = "";
+	private double rank = 0.0;
 	private int userId = 0;
-	private Date creationDate = null;
+	private int productId = 0;
+ 	private Date creationDate = null;
 	
 	public Review() {
 		
 	}
 	
+	public Review(int id, String title, String text, double rank, int userId, int productId, Date creationDate) {
+		this.id = id;
+		this.title = title;
+		this.text = text;
+		this.rank = rank;
+		this.userId = userId;
+		this.productId = productId;
+		this.creationDate = creationDate;
+	}
+	
 	static public ArrayList<Review> fetchAllProductReview(int productId) {
 		Connection conn = new MariaDbConnection().getConn();
+		ArrayList<Review> results = new ArrayList<Review>();
 		if (conn != null) {
-			String query = "SELECT * FROM review WHERE "
+			String query = "SELECT * FROM review WHERE productID=?";
+			try {
+				PreparedStatement stmt = conn.prepareStatement(query);
+				stmt.setInt(1, productId);
+				ResultSet rs = stmt.executeQuery();
+				while (rs.next()) {
+					results.add(new Review(rs.getInt("ID"), rs.getString("title"), rs.getString("text"), rs.getDouble("rank"), rs.getInt("userId"), rs.getInt("productId"), rs.getDate("creationDate")));
+				}
+				return results;
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
 		}
-		return null;
+		return results;
 	}
 }
